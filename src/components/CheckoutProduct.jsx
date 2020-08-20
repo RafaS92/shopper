fd635a961973f7839ac857f269707abe4c725055
RafaS92 <manrvaldez..92@gmail.com>
2020-08-20 18:02:56 -0500

    displayin products style

--- src/components/CheckoutProduct.jsx
+++ src/components/CheckoutProduct.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import "../CheckoutProduct.css";
 
 function CheckoutProduct({ id, item, title, image, price, rating }) {
   return (
     <div className="checkoutProduct">
-      <img src={image} alt="" />
+      <img className="checkoutProduct_image " src={image} alt="" />
+      <div className="checkoutProduct_info">
+        <p className="checkoutProduct_title">{title}</p>
+        <p className="checkoutProduct_price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product_rating">
+          {Array(rating)
+            .fill()
+            .map((_) => (
+              <p>&#11088;</p>
+            ))}
+        </div>
+
+        <button>Remove</button>
+      </div>
     </div>
   );
 }
