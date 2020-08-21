71c917afd60139209ac8c4a4f82aa9ae6ad41753
RafaS92 <manrvaldez..92@gmail.com>
2020-08-21 18:13:09 -0500

    subtotal component created and styled

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { useStateValue } from "../StateProvider";
 import "../Checkout.css";
 import CheckoutProduct from "./CheckoutProduct";
+import Subtotal from "./Subtotal";
 
 function Checkout() {
   const [{ basket }] = useStateValue();
 
   return (
     <div className="checkout">
-      <div>
+      <div className="checkout_left">
         <img
           className="checkout_img"
           src="https://cdn11.bigcommerce.com/s-utiov5b794/product_images/uploaded_images/blog13-sell-to-gainsaver.jpg"
@@ -34,6 +35,10 @@ function Checkout() {
             ))}
           </div>
         )}
+      </div>
+
+      <div className="checkout_right">
+        <Subtotal />
       </div>
     </div>
   );
