0e9bf1e13086b98eb522a25ea60ea48bd60fa96d
RafaS92 <manrvaldez..92@gmail.com>
2020-08-22 08:09:03 -0500

    login complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Checkout from "./components/Checkout";
 import Home from "./components/Home";
 import "./App.css";
 import Login from "./components/Login";
+import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
 
 function App() {
+  const [{ user }, dispatch] = useStateValue();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  console.log(user);
   return (
     <Router>
       <div className="app">
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Login.css";
 import { Link, useHistory } from "react-router-dom";
+import { auth } from "../firebase";
 
 function Login() {
+  const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const login = (e) => {
     e.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((auth) => {
+        history.replace("/");
+      })
+
+      .catch((e) => alert(e.message));
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {})
+      .catch((e) => alert(e.message));
+    history.push("/");
   };
 
   return (
@@ -17,9 +40,17 @@ function Login() {
         <h1>Sign in</h1>
         <form>
           <h5>E-mail</h5>
-          <input type="email" />
+          <input
+            type="email"
+            onChange={(event) => setEmail(event.target.value)}
+            value={email}
+          />
           <h5>Password</h5>
-          <input type="password" />
+          <input
+            type="password"
+            onChange={(event) => setPassword(event.target.value)}
+            value={password}
+          />
           <button onClick={login} type="submit" className="login_signInButton">
             Sign In
           </button>
