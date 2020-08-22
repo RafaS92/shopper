d6f3c43726e5b7ae3e46a4482d65f566dbddc3d4
RafaS92 <manrvaldez..92@gmail.com>
2020-08-21 19:14:21 -0500

    icon created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,11 +11,7 @@ function Header() {
   return (
     <nav className="header">
       <Link to="/">
-        <img
-          className="header_logo"
-          src="https://miro.medium.com/max/396/0*bVnfVVG7Y7qXQcO1"
-          alt=""
-        />
+        <img className="header_logo" src="../images/shopper.png" alt="" />
       </Link>
       <div className="header_search">
         <input type="text" className="header_searchInput" />
--- src/components/Subtotal.jsx
+++ src/components/Subtotal.jsx
@@ -3,6 +3,7 @@ import Checkout from "./Checkout";
 import CurrencyFormat from "react-currency-format";
 import { useStateValue } from "../StateProvider";
 import "../Subtotal.css";
+import { getBasketTotal } from "../reducer";
 
 function Subtotal() {
   const [{ basket }, dispatch] = useStateValue();
@@ -20,7 +21,7 @@ function Subtotal() {
           </>
         )}
         decimalScale={2}
-        value={0}
+        value={getBasketTotal(basket)}
         displayType={"text"}
         thousandSeparator={true}
         prefix={"$"}
